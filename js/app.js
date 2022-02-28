6e555c92f0627b99f07e108c4d49d43934eaea70
dev-sibli <siblisadik13@gmail.com>
2022-02-28 22:13:29 +0600

    Fisrt function added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-alert();
+phoneSearch = () => {
+	const inputText = document.getElementById('input-value');
+	const searchText = inputText.value;
+	console.log(searchText);
+	// API Fetch via Link
+	const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => displayPhone(data.data));
+	inputText.value = '';
+};
+const displayPhone = (phone) => {
+	phone.forEach((phone) => {
+		console.log(phone);
+	});
+};
